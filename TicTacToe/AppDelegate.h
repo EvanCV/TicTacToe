c0e5a2e19de80c7c2ee302366ac06977c2113c27
EvanCV <evan.c.vandenberg@gmail.com>
2015-01-08 13:58:49 -0800

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Evan Vandenberg on 1/8/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
