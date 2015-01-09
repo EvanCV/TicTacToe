36bcf4cbda5e93b474d0f24571aca05ed3403f85
EvanCV <evan.c.vandenberg@gmail.com>
2015-01-08 22:13:42 -0800

    End of Thursday

--- TicTacToe/RootViewController.m
+++ TicTacToe/RootViewController.m
@@ -10,6 +10,21 @@
 
 @interface RootViewController ()
 
+//Mutable arrays which will be filled with the users actions
+@property NSMutableSet *setWithUserXInputs;
+@property NSMutableSet *setWithUserOInputs;
+
+//Winning condition sets.
+@property NSSet *allAnswerPossibilitiesSet;
+@property NSSet *winningCondition1;
+@property NSSet *winningCondition2;
+@property NSSet *winningCondition3;
+@property NSSet *winningCondition4;
+@property NSSet *winningCondition5;
+@property NSSet *winningCondition6;
+@property NSSet *winningCondition7;
+@property NSSet *winningCondition8;
+
 //Setting the property for each UIImageView in the tic tac toe
 //First row = A, Left most column = 1
 @property (weak, nonatomic) IBOutlet UIImageView *A1;
@@ -34,6 +49,7 @@
 @property BOOL currentPlayer;
 
 
+
 @end
 
 @implementation RootViewController
@@ -56,6 +72,28 @@
 
     //Initialize an array with all of the UIImageViews
     self.baseImageViewArray = [[NSMutableArray alloc]initWithObjects:self.A1, self.A2, self.A3, self.B1, self.B2, self.B3,self.C1, self.C2, self.C3, nil];
+
+    //Setting the winning conditions
+    //3 in a row vertically
+    self.winningCondition1 = [[NSSet alloc]initWithObjects:@"1",@"4", @"7", nil];
+    self.winningCondition2 = [[NSSet alloc]initWithObjects:@"2",@"5", @"8", nil];
+    self.winningCondition3 = [[NSSet alloc]initWithObjects:@"3",@"6", @"9", nil];
+
+    //3 in a row horizontally
+    self.winningCondition4 = [[NSSet alloc]initWithObjects:@"1",@"2", @"3", nil];
+    self.winningCondition5 = [[NSSet alloc]initWithObjects:@"4",@"5", @"6", nil];
+    self.winningCondition6 = [[NSSet alloc]initWithObjects:@"7",@"8", @"9", nil];
+
+    //3 in a row diaganol
+    self.winningCondition7 = [[NSSet alloc]initWithObjects:@"1",@"5", @"9", nil];
+    self.winningCondition8 = [[NSSet alloc]initWithObjects:@"3",@"5", @"7", nil];
+
+    //All of the winning conditions set as object in a comprehensive set
+    self.allAnswerPossibilitiesSet = [[NSSet alloc]initWithObjects:self.winningCondition1, self.winningCondition2, self.winningCondition3, self.winningCondition4, self.winningCondition5, self.winningCondition6, self.winningCondition7, self.winningCondition8, nil];
+
+    //Initialize user sets
+    self.setWithUserXInputs = [NSMutableSet new];
+    self.setWithUserOInputs = [NSMutableSet new];
 }
 
 //Creates random BOOL to set which player starts first
@@ -70,6 +108,17 @@
         return NO;
 }
 
+//comparing two sets to see if players moves = win
+- (BOOL) checkIfPlayerDidWin
+{
+    if ([self.setWithUserOInputs intersectsSet:self.allAnswerPossibilitiesSet] || [self.setWithUserXInputs intersectsSet:self.allAnswerPossibilitiesSet])
+    {
+        return YES;
+    }
+        return NO;
+}
+
+
 - (IBAction)onScreenTapped:(UIGestureRecognizer *)gesture
 {
 
@@ -79,15 +128,20 @@
     //For loop gets the frame property from all ImageViews then determines whether the CGPoint iis in any UIView frames. If the the CGPoint is in a frame it will place either an X or O image in the UIView depending on which user's turn it is.
     for (UIImageView *myImageView in self.baseImageViewArray)
     {
-        if (CGRectContainsPoint(myImageView.frame, touchPoint))
+        //Check to see if touch was in a frame and whether or not that frame is empty
+        if (CGRectContainsPoint(myImageView.frame, touchPoint) && myImageView.image == nil)
         {
             if (self.currentPlayer == YES)
             {
                 myImageView.image = [UIImage imageNamed:@"Xpicture"];
+                [self.setWithUserXInputs addObject:[NSString stringWithFormat:@"%li", (long)myImageView.tag]];
+                NSLog(@"%@",self.setWithUserXInputs.description);
             }
             if (self.currentPlayer == NO)
             {
                 myImageView.image = [UIImage imageNamed:@"Opicture"];
+                [self.setWithUserOInputs addObject:[NSString stringWithFormat:@"%li", (long)myImageView.tag]];
+                NSLog(@"%@",self.setWithUserOInputs.description);
             }
             if (gesture.state == UIGestureRecognizerStateEnded)
             {
@@ -102,7 +156,12 @@
                     self.whichPlayerLabel.text = @"Player O you're up!";
                 }
             }
-
+            if ([self checkIfPlayerDidWin])
+            {
+                [self.setWithUserXInputs removeAllObjects];
+                [self.setWithUserOInputs removeAllObjects];
+                self.whichPlayerLabel.text = @"game over";
+            }
         }
     }
 
